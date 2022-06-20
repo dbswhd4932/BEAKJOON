c3fc64d73cd479f0672643f709c5da2876964a7e
yoonjong <dbswhd4932@gmail.com>
2022-06-20 21:21:25 +0900

    22.06.20

--- src/BAEKJOON/NO1371X.java
+++ src/BAEKJOON/NO1371X.java
@@ -1,5 +1,6 @@
 package BAEKJOON;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NO1371X {
@@ -7,13 +8,28 @@ public class NO1371X {
 
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        // a ~ z = 26
         int[] arr = new int[26];
-
         char[] c = str.toCharArray();
-        for(int i=0; i<str.length(); i++){
 
+        for (int i = 0; i < str.length(); i++) {
+            if (c[i] == ' ') continue;
+            int idx = c[i] - 'a';
+            arr[idx]++;
         }
 
+        String ch = "";
+        int max = arr[0];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+                ch = String.valueOf((char)(i + 'a'));
+            } else if(max == arr[i]) {
+                ch += String.valueOf((char)(i + 'a'));
+            }
+        }
 
+        System.out.println(ch);
     }
 }
---
+++ src/BAEKJOON/NO2441.java
@@ -0,0 +1,21 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class NO2441 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n-i; j < n ; j++) {
+                System.out.print(" ");
+            }
+            for (int k = n-i; k > 0; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
